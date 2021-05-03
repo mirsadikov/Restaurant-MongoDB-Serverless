eec5b53ac25e17ec61c63e1033f5e7c7ed3b8ccf
Mirabror <mirabror9545@gmail.com>
2021-05-03 19:03:04 +0500

    Navbar fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
+        <a href="/" className="navbar-brand">
           Restaurant Reviews
         </a>
         <div className="navbar-nav mr-auto">
